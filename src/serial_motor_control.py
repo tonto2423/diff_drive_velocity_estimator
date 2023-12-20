65866bdba11dffd865169b61f110139417df2b24
tonto_2423 <tnt.kosen2423@gmail.com>
2023-12-20 17:15:52 +0900

    feat: モータのオンオフノード

---
+++ src/serial_motor_control.py
@@ -0,0 +1,60 @@
+import serial
+import time
+import serial.tools.list_ports
+import rospy
+from std_msgs.msg import Bool
+
+def find_arduino_uno_port():
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        if "Arduino Uno" in port.description:
+            return port.device
+    return None
+
+arduino_port = find_arduino_uno_port()
+if arduino_port is None:
+    print("Arduino Uno not found")
+else:
+    print(f"Arduino Uno found on port: {arduino_port}")
+    # ここでシリアル接続を開始
+    ser = serial.Serial(arduino_port, 9600, timeout=1)
+    ser.flush()
+ser.flush()
+
+# rosの初期化
+rospy.init_node('motor_on', anonymous=True)
+rate = rospy.Rate(10) # 10hz
+rospy.loginfo("motor_onを開始します。")
+motor_pub = rospy.Publisher('/motor_on', Bool, queue_size=10)
+
+def send_command(command):
+    ser.write(f"{command}\n".encode('utf-8'))
+
+def read_distance():
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        if line.startswith("Distance:"):
+            distance = float(line.split(" ")[1])
+            print(f"Distance: {distance} m")
+
+try:
+    while rospy.is_shutdown() is False:
+        command = input("Enter command (start/stop): ")
+        # Arduinoにコマンドを送信
+        if command == "start":
+            motor_pub.publish(True)
+        elif command == "stop":
+            motor_pub.publish(False)
+        else:
+            print(f"Invalid command: {command}")
+            continue
+        send_command(command)
+
+        # Arduinoからのデータを読み取る
+        read_distance()
+
+        rate.sleep() # 10hz
+        
+except KeyboardInterrupt:
+    ser.close()
+    print("Serial connection closed.")
