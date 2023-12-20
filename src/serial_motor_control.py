8e263ea72516867807e4404297e6c5e961ffbba2
tonto_2423 <tnt.kosen2423@gmail.com>
2023-12-20 17:29:38 +0900

    fix: コマンドラインからポート名指定

--- src/serial_motor_control.py
+++ src/serial_motor_control.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python3
 import serial
-import time
 import serial.tools.list_ports
 import rospy
 from std_msgs.msg import Bool
 
-def find_arduino_uno_port():
-    ports = serial.tools.list_ports.comports()
-    for port in ports:
-        if "Arduino Uno" in port.description:
-            return port.device
-    return None
-
-arduino_port = find_arduino_uno_port()
-if arduino_port is None:
-    print("Arduino Uno not found")
-else:
-    print(f"Arduino Uno found on port: {arduino_port}")
-    # ここでシリアル接続を開始
-    ser = serial.Serial(arduino_port, 9600, timeout=1)
-    ser.flush()
-ser.flush()
-
 # rosの初期化
 rospy.init_node('motor_on', anonymous=True)
+arduino_port = rospy.get_param('~arduino_port', '/dev/ttyACM0')  # パラメータを取得、デフォルト値は '/dev/ttyACM0'
 rate = rospy.Rate(10) # 10hz
 rospy.loginfo("motor_onを開始します。")
 motor_pub = rospy.Publisher('/motor_on', Bool, queue_size=10)
+
+# def find_arduino_uno_port():
+#     ports = serial.tools.list_ports.comports()
+#     for port in ports:
+#         if "Arduino Uno" in port.description:
+#             return port.device
+#     return None
+
+# arduino_port = find_arduino_uno_port()
+# if arduino_port is None:
+#     print("Arduino Uno not found")
+# else:
+#     print(f"Arduino Uno found on port: {arduino_port}")
+#     # ここでシリアル接続を開始
+#     ser = serial.Serial(arduino_port, 9600, timeout=1)
+#     ser.flush()
+
+ser = serial.Serial(arduino_port, 9600, timeout=1)
+ser.flush()
 
 def send_command(command):
     ser.write(f"{command}\n".encode('utf-8'))
